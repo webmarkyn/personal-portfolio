466a7e55ae0f37a0d1bcb512967b6e4024310d45
webmarkyn <web.markyn@gmail.com>
2020-02-27 01:34:29 +0300

    project done

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 import './scss/app.scss';
+const SmoothScroll = require('smooth-scroll')
+
+const scroll = new SmoothScroll('a[href*="#"]');

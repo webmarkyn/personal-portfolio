5000fd0bbaa86f23278b03d507270dc42b4ef710
webmarkyn <web.markyn@gmail.com>
2020-04-15 23:02:37 +0300

    Fixed mobile version and added javascript to handle scroll

--- src/index.js
+++ src/index.js
@@ -5,7 +5,37 @@ import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 import './scss/app.scss';
 const SmoothScroll = require('smooth-scroll');
-const scroll = new SmoothScroll('a[href*="#"]');
+const navLinks = [...document.querySelectorAll('a[href*="#"]')];
+const scroll = new SmoothScroll();
+const header = document.getElementById('header');
+const heroes = [...document.querySelectorAll('.hero')];
+let currentHero = null;
+
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+
+navLinks.forEach((link) => {
+  link.onclick = (e) => {
+    e.preventDefault();
+    console.log(e.target)
+    if (e.target.localName !== 'a') makeLinkActive(e.target.parentElement);
+    makeLinkActive(e.target);
+  }
+});
+
+const disableAllLinks = (links) => {
+  links.forEach((link) => link.classList.remove('active'));
+};
+
+const makeLinkActive = (link) => {
+  disableAllLinks(navLinks);
+  link.classList.add('active');
+  const id = link.href.split('#')[1];
+  const el = document.getElementById(id);
+  currentHero = el;
+  scroll.animateScroll(el);
+};
 
 const allLinks = [...document.querySelectorAll('a')];
 allLinks.map(link => {
@@ -13,3 +43,41 @@ allLinks.map(link => {
     link.target = "_blank"
   }
 });
+
+const scrollDown = (index) => {
+  if (index === heroes.length-1) return;
+  if (index < 0) {
+    scroll.animateScroll(heroes[0], null, {speed: 500});
+    makeLinkActive(document.querySelector(`a[href="#${heroes[0].id}"]`));
+    return;
+  }
+  scroll.animateScroll(heroes[index+1], null, {speed: 500});
+  makeLinkActive(document.querySelector(`a[href="#${heroes[index+1].id}"]`));
+};
+
+const scrollUp = (index) => {
+  if (index <= 0) {
+    currentHero = null;
+    disableAllLinks(navLinks);
+    scroll.animateScroll(header, null, {speed: 500});
+    return;
+  }
+  scroll.animateScroll(heroes[index-1], null, {speed: 500});
+  makeLinkActive(document.querySelector(`a[href="#${heroes[index-1].id}"]`));
+};
+
+const enableScrollEvent = () => {
+  let time = Date.now();
+  window.addEventListener('wheel', (e) => {
+    if (window.innerWidth <= 1366) return
+    e.preventDefault();
+    if ((time + 1000 - Date.now()) < 0) {
+      const index = heroes.indexOf(currentHero);
+      if (Math.sign(e.deltaY) < 0) scrollUp(index);
+      else scrollDown(index);
+      time = Date.now()
+    }
+  }, {passive: false});
+};
+
+enableScrollEvent();
